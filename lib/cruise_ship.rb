3769af126f501f1e4e66c0a7dbfc65bcd8acd6cb
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-05 11:50:51 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,10 +1,10 @@
-# passengers = {
-# suite_a: "Amanda Presley",
-# suite_b: "Seymour Hoffman",
-# suite_c: "Alfred Tennyson",
-# suite_d: "Charlie Chaplin",
-# suite_e: "Crumpet the Elf"
-# }
+ passengers = {
+ suite_a: "Amanda Presley",
+ suite_b: "Seymour Hoffman",
+ suite_c: "Alfred Tennyson",
+ suite_d: "Charlie Chaplin",
+ suite_e: "Crumpet the Elf"
+ }
 
 def select_winner(passengers)
   # add the code snippet here!
@@ -12,7 +12,10 @@ def select_winner(passengers)
   passengers.each do |room,name|
     if room == "suite_a" && name.start_with?("A")
       lucky_winner = name
+      puts lucky_winner
     end
   end
   lucky_winner
 end
+
+select_winner(passengers)
