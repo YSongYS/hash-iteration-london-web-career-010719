62b730315d278a3c26fd7ff8fed2c2dce30f22f3
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-05 11:51:32 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -12,7 +12,7 @@ def select_winner(passengers)
   passengers.each do |room,name|
     puts room
     puts name
-    if room == "suite_a" && name.start_with?("A")
+    if room == ":suite_a" && name.start_with?("A")
       lucky_winner = name
       puts lucky_winner
     end
